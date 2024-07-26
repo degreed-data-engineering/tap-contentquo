f3a4ce26dda27364d72fbac6949a393d5c5fe58a
merveeren <meren@degreed.com>
2024-07-26 14:57:02 +0100

    initial

--- tap_contentquo/streams.py
+++ tap_contentquo/streams.py
@@ -1,5 +1,3 @@
-"""Stream class for tap-contentquo."""
-
 import base64
 import json
 from typing import Dict, Optional, Any, Iterable
@@ -9,6 +7,7 @@ from functools import cached_property
 from singer_sdk import typing as th
 from singer_sdk.streams import RESTStream
 from singer_sdk.authenticators import SimpleAuthenticator
+from singer_sdk.exceptions import FatalAPIError
 import requests
 
 
@@ -16,158 +15,191 @@ SCHEMAS_DIR = Path(__file__).parent / Path("./schemas")
 
 
 class TapContentQuoStream(RESTStream):
+    """ContentQuo stream class."""
 
     _LOG_REQUEST_METRIC_URLS: bool = True
 
     @property
     def url_base(self) -> str:
         """Base URL of source"""
-        return f"https://api.datadoghq.com"
+        return "https://degreed.contentquo.com/api/v1"
 
     @property
     def http_headers(self) -> dict:
-        """Return the http headers needed."""
-        headers = {}
-        headers["Content-Type"] = "application/json"
-        headers["Accept"] = "application/json"
+        """Return the HTTP headers needed."""
+        headers = {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        }
         return headers
 
     @property
     def authenticator(self):
-        http_headers = {}
-
-        # If only api_token is provided, use "Basic 123456789abcdefghijklmnopqrstuv" authentication
-        if (
-            self.config.get("api_token")
-            and not self.config.get("api_key")
-            and not self.config.get("app_key")
-        ):
-            http_headers["Authorization"] = "Basic " + self.config.get("api_token")
-
-        # If api and app keys are provided, and POST required:
-        if self.config.get("api_key") and self.config.get("app_key"):
-            http_headers["DD-API-KEY"] = self.config.get("api_key")
-            http_headers["DD-APPLICATION-KEY"] = self.config.get("app_key")
-
-        return SimpleAuthenticator(stream=self, auth_headers=http_headers)
-
-
-class Events(TapContentQuoStream):
-    name = "events"  # Stream name
-    path = "/api/v2/logs/events/search"  # API endpoint after base_url
-    primary_keys = ["id"]
-    records_jsonpath = "$.data[*]"  # https://jsonpath.com Use requests response json to identify the json path
-    replication_key = None
-    # schema_filepath = SCHEMAS_DIR / "events.json"  # Optional: use schema_filepath with .json inside schemas/
-
-    # Optional: If using schema_filepath, remove the propertyList schema method below
-    schema = th.PropertiesList(
-        th.Property("id", th.NumberType),
-        th.Property("name", th.StringType),
-    ).to_dict()
-    # Overwrite GET here by updating rest_method
-    rest_method = "POST"
-
-    def prepare_request_payload(
-        self, context: Optional[dict], next_page_token: Optional[Any]
-    ) -> Optional[dict]:
-        """Define request parameters to return"""
-        payload = {
-            "filter": {
-                "query": "source:degreed.api env:production",
-                "from": self.config.get("start_date"),
-            },
-            "page": {"limit": 4},
+        auth_url = f"{self.url_base}/auth/authenticate"
+        auth_payload = {
+            "key": self.config.get("api_key"),
+            "secret": self.config.get("api_secret"),
         }
-        return payload
 
-    # For passing url parameters:
-    # def get_url_params(
-    #     self, context: Optional[dict], next_page_token: Optional[Any]
-    # ) -> Dict[str, Any]:
+        response = requests.post(auth_url, json=auth_payload)
+        if response.status_code == 200:
+            token = response.json().get("token")
+            auth_headers = {"X-Auth-Token": token}
+            return SimpleAuthenticator(stream=self, auth_headers=auth_headers)
+        else:
+            raise Exception(
+                f"Failed to authenticate. Status code: {response.status_code}"
+            )
 
 
-### Template to use for new stream
-# class TemplateStream(RESTStream):
-#     """Template stream class."""
+class Evaluations(TapContentQuoStream):
+    name = "evaluations"  # Stream name
+    path = "/evaluations"  # API endpoint after base_url
+    primary_keys = ["eid"]
+    records_jsonpath = (
+        "$.evaluations[*]"  # Use requests response JSON to identify the JSON path
+    )
+    replication_key = None
 
-#     # TODO: Set the API's base URL here:
-#     url_base = "https://api.mysample.com"
+    schema = th.PropertiesList(
+        th.Property("analyticalIssueCount", th.IntegerType),
+        th.Property("eid", th.StringType),
+        th.Property("name", th.StringType),
+        th.Property("created", th.StringType),
+        th.Property("currentStep", th.StringType),
+        th.Property("editCount", th.IntegerType),
+        th.Property("groupID", th.IntegerType),
+        th.Property("groupName", th.StringType),
+        th.Property("profileID", th.StringType),
+        th.Property("projectID", th.IntegerType),
+        th.Property(
+            "qualityResult",
+            th.ObjectType(
+                th.Property(
+                    "analytical",
+                    th.ObjectType(
+                        th.Property("gradeIndex", th.IntegerType),
+                        th.Property("gradeName", th.StringType),
+                        th.Property("score", th.NumberType),
+                    ),
+                ),
+                th.Property(
+                    "auto",
+                    th.ObjectType(
+                        th.Property("characTer", th.StringType),
+                        th.Property("tausEditDensity", th.StringType),
+                    ),
+                ),
+                th.Property("holistic", th.StringType),
+            ),
+        ),
+        th.Property(
+            "scope",
+            th.ObjectType(
+                th.Property("fileCount", th.IntegerType),
+                th.Property("volume", th.IntegerType),
+            ),
+        ),
+        th.Property("scoredAnalyticalIssueCount", th.IntegerType),
+        th.Property("srcLocale", th.StringType),
+        th.Property("started", th.StringType),
+        th.Property("tgtLocale", th.StringType),
+        th.Property("translatorID", th.IntegerType),
+        th.Property("workflowID", th.IntegerType),
+        th.Property("workflowName", th.StringType),
+    ).to_dict()
 
-#     # OR use a dynamic url_base:
-#     # @property
-#     # def url_base(self) -> str:
-#     #     """Return the API URL root, configurable via tap settings."""
-#     #     return self.config["api_url"]
+    def get_child_context(self, record: dict, context: Optional[dict]) -> dict:
+        """Return a context dictionary for child streams."""
+        return {"eid": record["eid"]}
 
-#     records_jsonpath = "$[*]"  # Or override `parse_response`.
-#     next_page_token_jsonpath = "$.next_page"  # Or override `get_next_page_token`.
 
-#     @property
-#     def authenticator(self) -> BasicAuthenticator:
-#         """Return a new authenticator object."""
-#         return BasicAuthenticator.create_for_stream(
-#             self,
-#             username=self.config.get("username"),
-#             password=self.config.get("password"),
-#         )
+class EvaluationDetails(TapContentQuoStream):
+    name = "evaluation_details"  # Stream name
+    parent_stream_type = Evaluations
+    path = "/evaluations/{eid}"  # API endpoint after base_url
+    primary_keys = ["id"]
+    records_jsonpath = "$"  # Use requests response JSON to identify the JSON path
+    replication_key = None
 
-#     @property
-#     def http_headers(self) -> dict:
-#         """Return the http headers needed."""
-#         headers = {}
-#         if "user_agent" in self.config:
-#             headers["User-Agent"] = self.config.get("user_agent")
-#         # If not using an authenticator, you may also provide inline auth headers:
-#         # headers["Private-Token"] = self.config.get("auth_token")
-#         return headers
+    schema = th.PropertiesList(
+        th.Property("id", th.StringType),
+        th.Property("analyticalIssueCount", th.IntegerType),
+        th.Property(
+            "assignees",
+            th.ArrayType(
+                th.ObjectType(
+                    th.Property(
+                        "assignments",
+                        th.ArrayType(
+                            th.ObjectType(
+                                th.Property("role", th.StringType),
+                            )
+                        ),
+                    ),
+                    th.Property("fullname", th.StringType),
+                    th.Property("userId", th.IntegerType),
+                )
+            ),
+        ),
+        th.Property("comments", th.StringType),
+        th.Property("created", th.StringType),
+        th.Property("currentStep", th.StringType),
+        th.Property("editCount", th.IntegerType),
+        th.Property("groupID", th.IntegerType),
+        th.Property("groupName", th.StringType),
+        th.Property("name", th.StringType),
+        th.Property("profileID", th.StringType),
+        th.Property("projectID", th.IntegerType),
+        th.Property(
+            "qualityResult",
+            th.ObjectType(
+                th.Property(
+                    "analytical",
+                    th.ObjectType(
+                        th.Property("gradeIndex", th.IntegerType),
+                        th.Property("gradeName", th.StringType),
+                        th.Property("score", th.NumberType),
+                    ),
+                ),
+                th.Property(
+                    "auto",
+                    th.ObjectType(
+                        th.Property("characTer", th.StringType),
+                        th.Property("tausEditDensity", th.StringType),
+                    ),
+                ),
+                th.Property("holistic", th.StringType),
+            ),
+        ),
+        th.Property(
+            "scope",
+            th.ObjectType(
+                th.Property("fileCount", th.IntegerType),
+                th.Property("volume", th.IntegerType),
+            ),
+        ),
+        th.Property("scoredAnalyticalIssueCount", th.IntegerType),
+        th.Property("srcLocale", th.StringType),
+        th.Property("started", th.StringType),
+        th.Property("tgtLocale", th.StringType),
+        th.Property("translatorID", th.IntegerType),
+        th.Property("workflowID", th.IntegerType),
+        th.Property("workflowName", th.StringType),
+    ).to_dict()
 
-#     def get_next_page_token(
-#         self, response: requests.Response, previous_token: Optional[Any]
-#     ) -> Optional[Any]:
-#         """Return a token for identifying next page or None if no more pages."""
-#         # TODO: If pagination is required, return a token which can be used to get the
-#         #       next page. If this is the final page, return "None" to end the
-#         #       pagination loop.
-#         if self.next_page_token_jsonpath:
-#             all_matches = extract_jsonpath(
-#                 self.next_page_token_jsonpath, response.json()
-#             )
-#             first_match = next(iter(all_matches), None)
-#             next_page_token = first_match
-#         else:
-#             next_page_token = response.headers.get("X-Next-Page", None)
+    def post_process(self, row: dict, context: Optional[dict]) -> dict:
+        row["eid"] = context["eid"]
+        return row
 
-#         return next_page_token
-
-#     def get_url_params(
-#         self, context: Optional[dict], next_page_token: Optional[Any]
-#     ) -> Dict[str, Any]:
-#         """Return a dictionary of values to be used in URL parameterization."""
-#         params: dict = {}
-#         if next_page_token:
-#             params["page"] = next_page_token
-#         if self.replication_key:
-#             params["sort"] = "asc"
-#             params["order_by"] = self.replication_key
-#         return params
-
-#     def prepare_request_payload(
-#         self, context: Optional[dict], next_page_token: Optional[Any]
-#     ) -> Optional[dict]:
-#         """Prepare the data payload for the REST API request.
-
-#         By default, no payload will be sent (return None).
-#         """
-#         # TODO: Delete this method if no payload is required. (Most REST APIs.)
-#         return None
-
-#     def parse_response(self, response: requests.Response) -> Iterable[dict]:
-#         """Parse the response and return an iterator of result records."""
-#         # TODO: Parse response body and return a set of records.
-#         yield from extract_jsonpath(self.records_jsonpath, input=response.json())
-
-#     def post_process(self, row: dict, context: Optional[dict]) -> dict:
-#         """As needed, append or transform raw data to match expected structure."""
-#         # TODO: Delete this method if not needed.
-#         return row
+    def request_records(self, context: Optional[dict]) -> Iterable[dict]:
+        """Request records for the stream, handling 404 errors specifically."""
+        try:
+            yield from super().request_records(context)
+        except FatalAPIError as e:
+            if "404 Client Error" in str(e):
+                self.logger.warn(
+                    f"Evaluation ID {context.get('eid')} not found. Skipping."
+                )
+            else:
+                raise
